840885469da6d0fa42052982079b5d82443aa30c
Maciej Brodecki <ma.brodecki@binarapps.com>
2015-04-26 15:01:59 +0200

    fix first table

--- app/controllers/dual_tasks_controller.rb
+++ app/controllers/dual_tasks_controller.rb
@@ -4,16 +4,16 @@ class DualTasksController < ApplicationController
   def calculation
     @params = params
     hash = DualSimplex.uzupelnij(params)
-    @sympleks_tab=[[],[],[],[],[],[],[]]
-    @sympleks_tab=hash[:sympleks_tab]
-    @start_tab = hash[:sympleks_tab]
+    sympleks_tab=[[],[],[],[],[],[],[]]
+    sympleks_tab = hash[:sympleks_tab]
+    @start_tab = [hash[:sympleks_tab][0].clone, hash[:sympleks_tab][1].clone,hash[:sympleks_tab][2].clone,hash[:sympleks_tab][3].clone,hash[:sympleks_tab][4].clone,hash[:sympleks_tab][5].clone,hash[:sympleks_tab][6].clone]
     # puts @sympleks_tab.inspect
     # DualSimplex.transformation_to_one_in_cell(@sympleks_tab)
     # puts @sympleks_tab.inspect
     # DualSimplex.transformation_other_to_zero(@sympleks_tab)
     # puts @sympleks_tab.inspect
-    result_hash = DualSimplex.all_in(@sympleks_tab)
-    @sympleks_tab = result_hash[:sympleks_tab]
+    result_hash = DualSimplex.all_in(sympleks_tab)
+    sympleks_tab = result_hash[:sympleks_tab]
     @step_by_step = result_hash[:step_by_step]
     @success = result_hash[:success]
     render :result
