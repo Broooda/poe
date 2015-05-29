079e422d492fa28000e34c59c2ac7a73bebe364f
groarq <groarq@groarq.pl>
2015-05-29 09:14:51 +0200

    sqlite

--- app/controllers/dual_tasks_controller.rb
+++ app/controllers/dual_tasks_controller.rb
@@ -7,7 +7,6 @@ class DualTasksController < ApplicationController
     sympleks_tab=[[],[],[],[],[],[],[], [], [], []]
     sympleks_tab = hash[:sympleks_tab]
     @start_tab = [hash[:sympleks_tab][0].clone, hash[:sympleks_tab][1].clone,hash[:sympleks_tab][2].clone,hash[:sympleks_tab][3].clone,hash[:sympleks_tab][4].clone,hash[:sympleks_tab][5].clone,hash[:sympleks_tab][6].clone,hash[:sympleks_tab][7].clone,hash[:sympleks_tab][8].clone,hash[:sympleks_tab][9].clone]
-
     @success=true
     begin
       result_hash = FourSimplex.all_in(sympleks_tab)
