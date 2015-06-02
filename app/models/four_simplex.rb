77ebe159756b1de5382f76e384ddef813736577d
warsztaty <warsztaty@debian>
2015-06-02 22:33:33 +0200

    powiedzmy ze dziala

--- app/models/four_simplex.rb
+++ app/models/four_simplex.rb
@@ -25,7 +25,7 @@ class FourSimplex < ActiveRecord::Base
     numerator[7] = 0.0
     numerator[8] = 0.0
     numerator[9] = 0.0
-    numerator[10] = 0.0
+    #numerator[10] = 0.0
 
     denominator[0] = ''
     denominator[1] = ''
@@ -37,7 +37,7 @@ class FourSimplex < ActiveRecord::Base
     denominator[7] = 0.0
     denominator[8] = 0.0
     denominator[9] = 0.0
-    denominator[10] = 0.0
+    #denominator[10] = 0.0
 
     sympleks_tab[0][0] = ''
     sympleks_tab[0][1] = ''
@@ -47,9 +47,9 @@ class FourSimplex < ActiveRecord::Base
     sympleks_tab[0][5] = 0.0
     sympleks_tab[0][6] = 0.0
     sympleks_tab[0][7] = 0.0
-    sympleks_tab[0][8] = 0.0#-1.0 # U NAS 0.0, dla ywkladu -1.0
+    sympleks_tab[0][8] = 0.0
     sympleks_tab[0][9] = 0.0
-    sympleks_tab[0][10] = 0.0#-1.0  # U NAS TRZEBA ZAMIENIC 
+    #sympleks_tab[0][10] = 0.0#-1.0  # U NAS TRZEBA ZAMIENIC 
 
     sympleks_tab[1][0] = ''
     sympleks_tab[1][1] = ''
@@ -61,7 +61,7 @@ class FourSimplex < ActiveRecord::Base
     sympleks_tab[1][7] = 0.0
     sympleks_tab[1][8] = 0.0
     sympleks_tab[1][9] = 0.0
-    sympleks_tab[1][10] = 0.0
+    #sympleks_tab[1][10] = 0.0
 
     sympleks_tab[2][0]='cB'
     sympleks_tab[2][1]='dB'
@@ -73,7 +73,7 @@ class FourSimplex < ActiveRecord::Base
     sympleks_tab[2][7]='p4'
     sympleks_tab[2][8]='p5'
     sympleks_tab[2][9]='p6'
-    sympleks_tab[2][10]='p7'
+    #sympleks_tab[2][10]='p7'
 
     sympleks_tab[3][0]=0.0
     sympleks_tab[3][1]=0.0
@@ -85,11 +85,11 @@ class FourSimplex < ActiveRecord::Base
     sympleks_tab[3][7]=0.0
     sympleks_tab[3][8]=0.0
     sympleks_tab[3][9]=0.0
-    sympleks_tab[3][10] = 0.0
+    #sympleks_tab[3][10] = 0.0
 
     sympleks_tab[4][0]= -1.0
     sympleks_tab[4][1]=0.0
-    sympleks_tab[4][2]='p4'
+    sympleks_tab[4][2]='p7'
     sympleks_tab[4][3]=params[:bx3].to_f
     sympleks_tab[4][4]=params[:bx1].to_f
     sympleks_tab[4][5]=params[:bx2].to_f
@@ -97,11 +97,11 @@ class FourSimplex < ActiveRecord::Base
     sympleks_tab[4][7]=symbolToTable(params[:b_symbol])
     sympleks_tab[4][8]=0.0
     sympleks_tab[4][9]=0.0
-    sympleks_tab[4][10] = 0.0
+    #sympleks_tab[4][10] = 0.0
 
-    sympleks_tab[5][0]=0.0
+    sympleks_tab[5][0]=-1.0
     sympleks_tab[5][1]=0.0
-    sympleks_tab[5][2]='p5'
+    sympleks_tab[5][2]='p8'
     sympleks_tab[5][3]=params[:cx3].to_f
     sympleks_tab[5][4]=params[:cx1].to_f
     sympleks_tab[5][5]=params[:cx2].to_f
@@ -109,11 +109,11 @@ class FourSimplex < ActiveRecord::Base
     sympleks_tab[5][7]=0.0
     sympleks_tab[5][8]=symbolToTable(params[:c_symbol])
     sympleks_tab[5][9]=0.0
-    sympleks_tab[5][10] = 0.0
+    #sympleks_tab[5][10] = 0.0
 
     sympleks_tab[6][0]=0.0
     sympleks_tab[6][1]=0.0
-    sympleks_tab[6][2]='p6'
+    sympleks_tab[6][2]='p9'
     sympleks_tab[6][3]=params[:dx3].to_f
     sympleks_tab[6][4]=params[:dx1].to_f
     sympleks_tab[6][5]=params[:dx2].to_f
@@ -121,7 +121,7 @@ class FourSimplex < ActiveRecord::Base
     sympleks_tab[6][7]=0.0
     sympleks_tab[6][8]=0.0
     sympleks_tab[6][9]=symbolToTable(params[:d_symbol])
-    sympleks_tab[6][10] = 0.0
+    #sympleks_tab[6][10] = 0.0
 
     sympleks_tab[7][0]=''
     sympleks_tab[7][1]='delta'
@@ -140,13 +140,13 @@ class FourSimplex < ActiveRecord::Base
   end
 
   def self.calculate_deltas(sympleks_tab)
-    (3..10).each do |c|
+    (3..9).each do |c|
       sympleks_tab[7][c] = 0.0
       sympleks_tab[8][c] = 0.0 
       sympleks_tab[9][c] = 0.0
     end
 
-    (3..10).each do |i|
+    (3..9).each do |i|
       (3..6).each do |j|
         sympleks_tab[7][i] += sympleks_tab[j][0] * sympleks_tab[j][i]
         sympleks_tab[8][i] += sympleks_tab[j][1] * sympleks_tab[j][i]
@@ -159,11 +159,12 @@ class FourSimplex < ActiveRecord::Base
         sympleks_tab[8][i] -= sympleks_tab[1][i]
       end
     end
-    (3..6).each do |i|
+    (3..9).each do |i|
       if i == 3
         sympleks_tab[9][i] = sympleks_tab[7][i] / sympleks_tab[8][i]
       else
         sympleks_tab[9][i] = sympleks_tab[7][3] * sympleks_tab[8][i] - (sympleks_tab[8][3] * sympleks_tab[7][i])
+        #puts "i: " +i.to_s+" "+sympleks_tab[9][i].to_s+" = "+sympleks_tab[7][3].to_s+" * "+sympleks_tab[8][i].to_s+" - ("+sympleks_tab[8][3].to_s+" * "+sympleks_tab[7][i].to_s+")"
       end
     end
   end
@@ -186,7 +187,7 @@ class FourSimplex < ActiveRecord::Base
   def self.find_enter_criteria(sympleks_tab)
      max = -89
      index = nil
-     (4..10).each do |i|
+     (4..9).each do |i|
         if sympleks_tab[9][i] > max
           max = sympleks_tab[9][i]
           index = i
@@ -205,7 +206,7 @@ class FourSimplex < ActiveRecord::Base
     sympleks_tab[y][1] = sympleks_tab[1][x]
 
     divider=sympleks_tab[y][x]
-    (3..10).each do |j|
+    (3..9).each do |j|
       sympleks_tab[y][j]=sympleks_tab[y][j]/divider
     end
     {sympleks_tab: sympleks_tab, x: x, y: y}
@@ -214,7 +215,7 @@ class FourSimplex < ActiveRecord::Base
   def self.transformation_other_to_zero(sympleks_tab, x, y)
     (3..9).each do |i|
       multiplier=(sympleks_tab[i][x])*(-1)
-     (3..10).each do |j|
+     (3..9).each do |j|
         sympleks_tab[i][j]=sympleks_tab[i][j]+(sympleks_tab[y][j]*multiplier) if i!=y
       end
     end
@@ -222,7 +223,7 @@ class FourSimplex < ActiveRecord::Base
   end
 
   def self.check_if_optimum(sympleks_tab)
-    (4..10).each do |i|
+    (4..9).each do |i|
       return false if sympleks_tab[9][i] > 0.0
       return false if sympleks_tab[8][i] > 0.0
       return false if sympleks_tab[7][i] > 0.0
@@ -232,7 +233,7 @@ class FourSimplex < ActiveRecord::Base
   
   def self.first_phase(sympleks_tab)
     step_by_step = []
-    (0..10).each do |c|
+    (0..9).each do |c|
       # binding.pry
       begin
         result = transformation_to_one_in_cell(sympleks_tab)
@@ -255,7 +256,7 @@ class FourSimplex < ActiveRecord::Base
     calculate_deltas(sympleks_tab)
     step_by_step = []
 
-    # (0..10).each do |c|
+     (0..10).each do |c|
       begin
         result = transformation_to_one_in_cell(sympleks_tab)
         x = result[:x]
@@ -269,17 +270,17 @@ class FourSimplex < ActiveRecord::Base
       rescue OptymalneException 
         return {sympleks_tab: sympleks_tab, step_by_step: step_by_step, success: true}
       end
-    # end
+     end
     {sympleks_tab: sympleks_tab, step_by_step: step_by_step, success: false}
   end
 
 
   def self.change_numerator_denominator(sympleks_tab, numerator, denominator)
-    (0..10).each do |c|
+    (0..9).each do |c|
       sympleks_tab[0][c] = numerator[c]
       sympleks_tab[1][c] = denominator[c]
     end
-    (3..10).each do |i|
+    (3..9).each do |i|
       (3..6).each do |j|
         if sympleks_tab[2][i] == sympleks_tab[j][2]
           sympleks_tab[j][0] = sympleks_tab[0][i]
